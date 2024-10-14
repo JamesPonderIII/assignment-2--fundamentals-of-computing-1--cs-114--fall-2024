42d40e731eb23aaeee0a5b2fa4074db47a00d18b
JamesPonderIII <jhponderiii@gmail.com>
2024-10-14 16:54:29 -0400

    Update Diamond.java

--- Diamond.java
+++ Diamond.java
@@ -4,18 +4,18 @@ public class Diamond {
     System.out.println("Enter a number");
     //Initiate the scanner
     Scanner sc=new Scanner(System.in);
-    int no=sc.nextInt();
+    int numb=sc.nextInt();
 
     int count=1;
-    for(int incrmntvalues=1;incrmntvalues<=2*no-1;incrmntvalues++) {
-      for(int secondincrmnt=count;secondincrmnt<=no;secondincrmnt++) {
+    for(int incrmntvalues=1;incrmntvalues<=2*numb-1;incrmntvalues++) {
+      for(int secondincrmnt=count;secondincrmnt<=numb;secondincrmnt++) {
         System.out.print(" ");
       }
       for(int thirdincrmnt=1;thirdincrmnt<=count*2-1;thirdincrmnt++) {
         System.out.print("*");
       }
-      if(incrmntvalues<no)
-      //If 
+      if(incrmntvalues<numb)
+      //If the number is greater then begin decreasing
               count++;
               else
               count--;
